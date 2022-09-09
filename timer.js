19ddac6e7fe96193ecc74109eace78e616dcfb83
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-09-09 15:24:24 -0400

    debugged and worked on adding in dynamic date/year for the copyright

--- timer.js
+++ timer.js
@@ -8,40 +8,60 @@ window.onload = (event) => {
   const buttonStop = document.getElementById("button-stop");
   const buttonReset = document.getElementById("button-reset");
 
+  // Initiate variables to their values
   let seconds = 0;
-  let intervalID;
+  let output = "00:00:00";
+  let intervalID = {};
 
-  timerSeconds.innerText = "00:00:00";
+  // Display the text on the DOM as the string of 00:00:00
+  timerSeconds.textContent = output;
 
+  // When user clicks the start button execute code below/within => Start timer
   buttonStart.addEventListener("click", () => {
     console.log("START MY ENGINE");
+
+    // This function displays time format, gives you the incriment in real time / dynamically
     intervalID = setInterval(function () {
       seconds++;
       let hours = Math.floor(seconds / 3600);
       let mins = Math.floor(seconds / 60) - hours * 60;
       let secs = Math.floor(seconds % 60);
 
-      // padStart(how many positions, "what you want displayed") gives us the 00 counter.
-      const output =
+      // PadStart(How many positions?, "what you want displayed").
+      // This gives us the timer displayed as 00.
+      output =
         hours.toString().padStart(2, "0") +
         ":" +
         mins.toString().padStart(2, "0") +
         ":" +
         secs.toString().padStart(2, "0");
-      //   console.log(output);
+      // console.log(output);
       timerSeconds.textContent = output;
     });
   });
 
+  // buttonStop after using the setInterval, we use the clearInterval method to stop the timer and display the current time captured.
   buttonStop.addEventListener("click", () => {
     clearInterval(intervalID);
     console.log("STOP ME");
   });
 
+  // buttonReset is used to remove all the numbers/ timer that has been captured and restate null.. back to 00:00:00
   buttonReset.addEventListener("click", () => {
-    console.log("RESET");
+    // console.log("RESET");
+    seconds = 0;
+    output = "00:00:00";
+    console.log(output); // = 00:00:00
 
-    // Re-state variables again to "null" in theory
-    timerSeconds.innerText = "00:00:00";
+    // This is the way of appending text to the DOM
+    timerSeconds.innerText = output;
   });
+
+  // Dynamic Date-Year, this will change once a year.
+  const copyrightYear = document.getElementById("copyright-year");
+  const today = new Date();
+  const currentYear = today.getFullYear();
+
+  copyrightYear.innerText = currentYear;
+  console.log(currentYear);
 };
