82e1016f8e130e111a6fb1e21702ad2862026a9f
Claudio Miranda <claudio_769@hotmail.com>
2022-10-06 21:57:03 -0400

    Adjusted the logic for the flip-card game, cards now match based on ID

--- timer.js
+++ timer.js
@@ -1,76 +1,9 @@
-window.onload = (event) => {
-  console.log("page is fully loaded.");
-
-  // Append to DOM section
-  const timerSeconds = document.getElementById("seconds");
-
-  const buttonStart = document.getElementById("button-start");
-  const buttonStop = document.getElementById("button-stop");
-  const buttonReset = document.getElementById("button-reset");
-
-  // Initiate variables to their values
-  let seconds = 0;
-  let output = "00:00:00";
-  let intervalID = {};
-
-  // Display the text on the DOM as the string of 00:00:00
-  timerSeconds.textContent = output;
-
-  // When user clicks the start button execute code below/within => Start timer
-  buttonStart.addEventListener("click", () => {
-    console.log("START MY ENGINE");
-
-    // This function displays time format, gives you the incriment in real time / dynamically
-    intervalID = setInterval(function () {
-      seconds++;
-      let hours = Math.floor(seconds / 3600);
-      let mins = Math.floor(seconds / 60) - hours * 60;
-      let secs = Math.floor(seconds % 60);
-
-      // PadStart(How many positions?, "what you want displayed").
-      // This gives us the timer displayed as 00.
-      output =
-        hours.toString().padStart(2, "0") +
-        ":" +
-        mins.toString().padStart(2, "0") +
-        ":" +
-        secs.toString().padStart(2, "0");
-      // console.log(output);
-      timerSeconds.textContent = output;
-    });
-  });
-
-  // buttonStop after using the setInterval, we use the clearInterval method to stop the timer and display the current time captured.
-  buttonStop.addEventListener("click", () => {
-    clearInterval(intervalID);
-    console.log("STOP ME");
-  });
-
-  // buttonReset is used to remove all the numbers/ timer that has been captured and restate null.. back to 00:00:00
-  buttonReset.addEventListener("click", () => {
-    // console.log("RESET");
-    seconds = 0;
-    output = "00:00:00";
-    console.log(output); // = 00:00:00
-
-    // This is the way of appending text to the DOM
-    timerSeconds.innerText = output;
-  });
-
-  // Dynamic Date-Year, this will change once a year.
-  const copyrightYear = document.getElementById("copyright-year");
-  const today = new Date().getFullYear();
-
-  copyrightYear.innerText = today;
-};
-
 class MixAndMatch {
   constructor(totalTime, cards) {
     this.cardsArray = cards;
     this.totalTime = totalTime;
     this.timeRemaining = totalTime;
 
-    // these two below are just for the time remaining and amount of flips, extract from HTML
     this.timer = document.getElementById("time-remaining");
     this.ticker = document.getElementById("flips");
   }
@@ -95,6 +28,7 @@ class MixAndMatch {
 
     // Reset
     this.hideCards();
+
     // Reseting the text when there is a new game.
     this.timer.innerText = this.timeRemaining;
     this.ticker.innerText = this.totalClicks;
@@ -132,8 +66,6 @@ class MixAndMatch {
       this.ticker.innerText = this.totalClicks;
       card.classList.add("visible");
 
-      // double check that CURLY brace syntax doesn't effect outcome
-
       if (this.cardToCheck) {
         this.checkForMatchedCard(card);
       } else {
@@ -142,7 +74,6 @@ class MixAndMatch {
     }
   }
 
-  // double check that CURLY brace syntax doesn't effect outcome
   checkForMatchedCard(card) {
     if (this.getCardType(card) === this.getCardType(this.cardToCheck)) {
       this.cardMatch(card, this.cardToCheck);
@@ -153,16 +84,13 @@ class MixAndMatch {
     this.cardToCheck = null;
   }
 
-  // Fucntion to grab the className and be able to match it using the src img-file.
-  // is src[0] === src[0]
+  // Input Card and access through className => id => slice => id = id => match!
   getCardType(card) {
-    return card.getElementsByClassName("card-value")[0].src;
+    return card.getElementsByClassName("game__driver-img")[0].id.slice(2);
   }
 
   cardMatch(card1, card2) {
-    this.matchedCards.push(card1);
-    this.matchedCards.push(card2);
-    // this.matchedCards.push(card1, card2);???
+    this.matchedCards.push(card1, card2);
 
     card1.classList.add("matched");
     card2.classList.add("matched");
@@ -183,6 +111,19 @@ class MixAndMatch {
     }, 1000);
   }
 
+  // Victory Overlay
+  victory() {
+    clearInterval(this.countDown);
+    document.getElementById("victory-text").classList.add("visible");
+  }
+
+  // Game over Overlay
+  gameOver() {
+    clearInterval(this.countDown);
+    document.getElementById("game-over-text").classList.add("visible");
+  }
+
+  // Countdown timer expires = game over
   startCountDown() {
     return setInterval(() => {
       this.timeRemaining--;
@@ -191,16 +132,6 @@ class MixAndMatch {
         this.gameOver();
       }
     }, 1000);
-  }
-
-  victory() {
-    clearInterval(this.countDown);
-    document.getElementById("victory-text").classList.add("visible");
-  }
-
-  gameOver() {
-    clearInterval(this.countDown);
-    document.getElementById("game-over-text").classList.add("visible");
   }
 }
 
@@ -228,3 +159,11 @@ if (document.readyState === "loading") {
 } else {
   ready();
 }
+
+window.onload = () => {
+  // Dynamic Date-Year, this will change once a year.
+  const copyrightYear = document.getElementById("copyright-year");
+  const today = new Date().getFullYear();
+
+  copyrightYear.innerText = today;
+};
